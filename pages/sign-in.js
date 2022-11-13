a42d7f9efef782badcec4f855c9d161565205fe9
Allen Calderwood <calderwoodra1113@gmail.com>
2022-11-12 17:00:29 -0800

    Lint

--- pages/sign-in.js
+++ pages/sign-in.js
@@ -34,46 +34,46 @@ export default function SignUpPage() {
 
     if (showCode) {
       showCode
-          .confirm(code)
-          .then(result => {
-            const user = result.user;
-            alert("Signed in as " + user.phoneNumber);
-          })
-          .catch(error => {
-            setError("Failed to sign-in");
-          })
-          .finally(() => {
-            setLoading(false);
-          });
-      return;
-    }
-    signIn(phone)
-        .then(confirmationResult => {
-          setShowCode(confirmationResult);
+        .confirm(code)
+        .then(result => {
+          const user = result.user;
+          alert("Signed in as " + user.phoneNumber);
         })
         .catch(error => {
-          setError("Failed to find account");
-          window.recaptchaVerifier.render().then(function (widgetId) {
-            grecaptcha.reset(widgetId);
-            setAllowSignIn(false);
-          });
+          setError("Failed to sign-in");
         })
         .finally(() => {
           setLoading(false);
         });
+      return;
+    }
+    signIn(phone)
+      .then(confirmationResult => {
+        setShowCode(confirmationResult);
+      })
+      .catch(error => {
+        setError("Failed to find account");
+        window.recaptchaVerifier.render().then(function (widgetId) {
+          grecaptcha.reset(widgetId);
+          setAllowSignIn(false);
+        });
+      })
+      .finally(() => {
+        setLoading(false);
+      });
   }
 
   useEffect(() => {
     if (!window.recaptchaVerifier) {
       window.recaptchaVerifier = new RecaptchaVerifier(
-          "recaptcha-container",
-          {
-            size: "normal",
-            callback: response => {
-              setAllowSignIn(true);
-            },
+        "recaptcha-container",
+        {
+          size: "normal",
+          callback: response => {
+            setAllowSignIn(true);
           },
-          firebaseAuth
+        },
+        firebaseAuth
       );
       window.recaptchaVerifier.render().then(widgetId => {
         window.recaptchaWidgetId = widgetId;
@@ -104,45 +104,45 @@ export default function SignUpPage() {
   }
 
   return (
-      <Section outerClassName={"bg-blue-200 h-screen"} innerClassName={"px-4"}>
-        <div className={"flex flex-col h-screen justify-center items-center"}>
-          <Card className={"max-w-lg"}>
-            <div className={"flex flex-col justify-center items-center"}>
-              <Text style={TextStyle.med_header}>Login to account</Text>
-              <Text style={TextStyle.paragraph}>Enter your phone number to login or create a new account.</Text>
+    <Section outerClassName={"bg-blue-200 h-screen"} innerClassName={"px-4"}>
+      <div className={"flex flex-col h-screen justify-center items-center"}>
+        <Card className={"max-w-lg"}>
+          <div className={"flex flex-col justify-center items-center"}>
+            <Text style={TextStyle.med_header}>Login to account</Text>
+            <Text style={TextStyle.paragraph}>Enter your phone number to login or create a new account.</Text>
+            <Input
+              label={"Phone number"}
+              outerClassName={"mt-4"}
+              placeholder={"800-111-2222"}
+              onChange={setPhone}
+              value={phone}
+              autoComplete={"phone"}
+              error={!!showCode ? "" : error}
+            />
+            {!!showCode && (
               <Input
-                  label={"Phone number"}
-                  outerClassName={"mt-4"}
-                  placeholder={"800-111-2222"}
-                  onChange={setPhone}
-                  value={phone}
-                  autoComplete={"phone"}
-                  error={!!showCode ? "" : error}
+                value={code}
+                onChange={setCode}
+                label={"Confirmation code"}
+                outerClassName={"mt-4"}
+                placeholder={"********"}
+                autoComplete={"one-time-code"}
+                error={error}
               />
-              {!!showCode && (
-                  <Input
-                      value={code}
-                      onChange={setCode}
-                      label={"Confirmation code"}
-                      outerClassName={"mt-4"}
-                      placeholder={"********"}
-                      autoComplete={"one-time-code"}
-                      error={error}
-                  />
-              )}
-              <div className={"mt-4"}>
-                <div id={"recaptcha-container"}/>
-              </div>
-              <Button
-                  className={"mt-4"}
-                  disabled={isDisabled()}
-                  text={"Sign in"}
-                  loading={loading}
-                  onClick={onSignInClicked}
-              />
+            )}
+            <div className={"mt-4"}>
+              <div id={"recaptcha-container"} />
             </div>
-          </Card>
-        </div>
-      </Section>
+            <Button
+              className={"mt-4"}
+              disabled={isDisabled()}
+              text={"Sign in"}
+              loading={loading}
+              onClick={onSignInClicked}
+            />
+          </div>
+        </Card>
+      </div>
+    </Section>
   );
 }
